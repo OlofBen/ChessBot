3db0ff3de2bbcda18e01c9cedf6265851bfb94c7
OlofBen <mejladr@plats.se>
2022-03-09 10:15:15 +0100

    Implemented minmax

--- strategies.py
+++ strategies.py
@@ -3,10 +3,14 @@ Some example strategies for people who want to create a custom, homemade bot.
 And some handy classes to extend
 """
 
+from tracemalloc import Statistic
 import chess
 from chess.engine import PlayResult
 import random
+
+from pyparsing import White
 from engine_wrapper import EngineWrapper
+#from functools import cache
 
 
 class FillerEngine:
@@ -102,3 +106,68 @@ class FirstMove(ExampleEngine):
         moves = list(board.legal_moves)
         moves.sort(key=str)
         return PlayResult(moves[0], None)
+
+class MinMax(MinimalEngine):
+
+    def search(self, board, time_limit, ponder, draw_offered):
+        print("Thinking")
+
+        def evaluate(board):
+            sum = 0
+            piece_map = board.piece_map()
+            
+            for i in piece_map:
+                piece = piece_map[i]
+                playerFactor = 1 if piece.symbol().isupper() else -1
+                symbol = piece.symbol().lower()
+                if symbol == 'p': sum += 1*playerFactor
+                if symbol == 'q': sum += 9*playerFactor
+                if symbol == 'b': sum += 3.25*playerFactor
+                if symbol == 'k': sum += 3*playerFactor
+                if symbol == 'r': sum += 5*playerFactor
+            
+
+                
+
+                
+            return sum
+
+
+        def minMax(board, depth, isWhite):
+            """MinMax"""
+
+            if depth == 0 or board.is_game_over():
+                return evaluate(board)
+            else:
+                moves = list(board.legal_moves)
+                bestEval = -10000 if isWhite else 10000
+                for move in moves:
+                    test_board = chess.Board(fen=board.fen())
+                    test_board.push(move)
+                    eval = minMax(test_board, depth - 1, not isWhite)
+
+                    if isWhite:
+                        if eval > bestEval:
+                            bestEval = eval
+                    else:
+                          if eval < bestEval:
+                            bestEval = eval
+                return bestEval
+
+
+    
+        isWhite = True
+        moves = list(board.legal_moves)
+        bestEval = -10000 if isWhite else 10000
+        bestMove = moves[0]
+        for move in moves:
+            test_board = chess.Board(fen=board.fen())
+            test_board.push(move)
+            eval = minMax(test_board, 2, not isWhite)
+            if(eval > bestEval):
+                bestEval = eval
+                bestMove = move
+        print(bestEval)
+        return PlayResult(bestMove, None)
+    
+    
